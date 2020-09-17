0d910592e4d628899b629e02523d04b01f5d944f
tahuang1991 <taohuang@physics.tamu.edu>
2020-09-17 23:58:15 +0200

    add plotLikelihoodScan.py

---
+++ HH_WWbb/plotLikelihoodScan.py
@@ -0,0 +1,37 @@
+import ROOT
+import sys
+import os
+
+sys.argv.append( '-b' )
+
+
+def plotLikelihoodScan1D(mass, channel, variable, xmin, xmax, plotdir, suffix):
+    treename = "limit"
+
+    infile = "higgsCombineGGToX0ToHHTo2B2L2Nu_NNvsHME_MCstat_M800_MuMu_likelihoodscan_r_expectS1.MultiDimFit.mH800.root"
+
+    tree = ROOT.TChain(treename)
+    tree.Add(infile)
+
+    hist = ROOT.TProfile("hist", "likelihood scan", 100, xmin, xmax)
+
+    #tree.Draw("2*deltaNLL:"+variable+" >> hist", "2*deltaNLL<10")
+    tree.Draw("2*deltaNLL:"+variable+" >> hist")
+    hist.Print("ALL")
+
+
+    c1 = ROOT.TCanvas("c1","c1",600, 800)
+
+    c1.SetGridx()
+    c1.SetGridy()                                                                                                                      
+    c1.SetTickx()
+    c1.SetTicky()
+
+    hist.Draw()
+    hist.GetXaxis().SetTitle(variable)
+    hist.GetYaxis().SetTitle("2*deltaNLL")
+
+    c1.SaveAs(plotdir+"Likelihoodscan_2deltaNLL_{var}_M{mass}_{ch}_{suffix}.pdf".format(mass=mass, ch = channel, suffix=suffix,var=variable))
+
+
+plotLikelihoodScan1D(800, "MuMu", "r", -1.0, 8.0, "./", "MTandMT2_MJJ_NNvsHME")
